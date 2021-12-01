30c0353e0778649ea0902ad5c538526a4492bde5
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2021-12-01 07:35:25 +0530

    parallel condition to win -X

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,13 @@
 const boxes = document.querySelectorAll(".box");
 
-const a1 = document.querySelector("#a1");
-const a2 = document.querySelector("#a2");
-const a3 = document.querySelector("#a3");
-const b1 = document.querySelector("#b1");
-const b2 = document.querySelector("#b2");
-const b3 = document.querySelector("#b3");
-const c1 = document.querySelector("#c1");
-const c2 = document.querySelector("#c2");
-const c3 = document.querySelector("#c3");
+
 const turn_box = document.querySelector(".turn");
 const start_btn = document.querySelector("#start_btn");
 const p_again = document.querySelector("#p_again");
 
 
-const n1 = prompt("Name of player 1: ");
-const n2 = prompt("Name of player 2: ");
+// const n1 = prompt("Name of player 1: ");
+// const n2 = prompt("Name of player 2: ");
 
 // start game
 start_btn.addEventListener("click", () => {
@@ -23,36 +15,67 @@ start_btn.addEventListener("click", () => {
     p_again.style.display = "block";
      
     // first turn
-    const n = Math.floor(Math.random() *2);
-             if(n == 0){
-                turn_box.innerHTML = `It's ${n1}'s Turn.`
-            }else{
-                turn_box.innerHTML = `It's ${n2}'s Turn.`
-            };
+    // const n = Math.floor(Math.random() *2);
+    //          if(n == 0){
+    //             turn_box.innerHTML = `It's ${n1}'s Turn.`
+    //         }else{
+    //             turn_box.innerHTML = `It's ${n2}'s Turn.`
+    //         };
 
 let flag = 1;
     //  enabling click
      boxes.forEach((box) => {
          box.addEventListener("click", () => {
-
+             turn_box.innerHTML = "Player1"
+            
              if(flag == 1){
                  box.innerHTML = "X";
+                 let a1 = document.querySelector("#a1").innerHTML;
+                 let a2 = document.querySelector("#a2").innerHTML;
+                 let a3 = document.querySelector("#a3").innerHTML;
+                 let b1 = document.querySelector("#b1").innerHTML;
+                 let b2 = document.querySelector("#b2").innerHTML;
+                 let b3 = document.querySelector("#b3").innerHTML;
+                 let c1 = document.querySelector("#c1").innerHTML;
+                 let c2 = document.querySelector("#c2").innerHTML;
+                 let c3 = document.querySelector("#c3").innerHTML;
+
+                 // check
+                 if(a1 == "X" && a2 == "X" && a3 == "X"){
+                     console.log(" X wins");
+                 }else if(b1 == "X" && b2 == "X" && b3 == "X"){
+                     console.log("X wins");
+                 }else if(c1 == "X" && c2 == "X" && c3 == "X"){
+                     console.log("X wins");
+                 }else if(a1 == "X" && b1 == "X" && c1 == "X"){
+                     console.log(("X wins"));
+                 }else if(a2 == "X" && b2 == "X" && c2 == "X"){
+                    console.log(("X wins"));
+                }else if(a3 == "X" && b3 == "X" && c3 == "X"){
+                    console.log(("X wins"));
+                }
+
+
                  box.disabled = true;
+                 
+
                  flag = 0;
-                 if(n == 1){
-                    turn_box.innerHTML = `It's ${n1}'s Turn.`
-                }else{
-                    turn_box.innerHTML = `It's ${n2}'s Turn.`
-                };
+                //  if(n == 1){
+                //     turn_box.innerHTML = `It's ${n1}'s Turn.`
+                // }else{
+                //     turn_box.innerHTML = `It's ${n2}'s Turn.`
+                // };
+                turn_box.innerHTML = "Player2"
              }else{
                  box.innerHTML = "0";
                  box.disabled = true;
                  flag = 1;
-                 if(n == 0){
-                    turn_box.innerHTML = `It's ${n1}'s Turn.`
-                }else{
-                    turn_box.innerHTML = `It's ${n2}'s Turn.`
-                };
+                //  if(n == 0){
+                //     turn_box.innerHTML = `It's ${n1}'s Turn.`
+                // }else{
+                //     turn_box.innerHTML = `It's ${n2}'s Turn.`
+                // };
+                turn_box.innerHTML = "Player1"
              };
          });
      });
